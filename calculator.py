762afc1c8940878397cf21ae2efb70a1b6a3acef
georgeJuniorDev <georgeamorim45@gmail.com>
2026-09-23 22:26:19 +0200

    Add files via upload
    my fisrt calculator in python

---
+++ calculator.py
@@ -0,0 +1,23 @@
+num1 = float(input("Enter the first number: "))
+operator = input("Enter the mathematical symbol: ")
+num2 = float(input("Enter the second number: "))
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    print(num1 / num2)
+elif operator == "**":
+    print(num1 ** num2)
+
+
+
+
+
+
+
+
+
